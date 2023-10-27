fda38d535d1396fde90bd131ec22ba9b476b647c
MCTNB <149130176+mctnb@users.noreply.github.com>
2023-10-27 14:35:09 -0500

    Reformat javascript and python versions and added blocks.

--- main.py
+++ main.py
@@ -1,30 +1,46 @@
-# Needed start-up functions
-def calculate(x: number, y: number):
-    return (x / y) * 100
-# Needed start-up variables
-worth = 1
-bitcoin = 30000
-mctnb = 0
-percentage = calculate(worth, bitcoin)
-# Initate start-up
-basic.show_string("Welcome to MCTNB Blockchain")
-basic.show_string("0 = Mine, 1 = See Stats, 2 = Reset")
-basic.show_string("Starting now MCTNB is worth 0.003% of a bitcoin.")
 # Controls
 
 def on_pin_pressed_p0():
     global mctnb
     mctnb += 0.1
-    basic.show_number(mctnb)
+    if not (kelp):
+        basic.show_number(mctnb)
 input.on_pin_pressed(TouchPin.P0, on_pin_pressed_p0)
 
-def on_pin_pressed_p1():
-    basic.show_string("You own " + str(calculate(mctnb, bitcoin)) + "% of bitcoin.")
-    basic.show_string("You have " + str(mctnb) + " mctnb.")
-input.on_pin_pressed(TouchPin.P1, on_pin_pressed_p1)
+# Button Press
+
+def on_button_pressed_a():
+    global kelp
+    basic.show_string("Simple Mode")
+    basic.show_string("No longer will show number.")
+    kelp = True
+input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_pin_pressed_p2():
     global mctnb
     basic.show_string("RESET")
     mctnb = 0
 input.on_pin_pressed(TouchPin.P2, on_pin_pressed_p2)
+
+def on_pin_pressed_p1():
+    basic.show_string("You own " + str(calculate(mctnb, bitcoin)) + "% of bitcoin.")
+    basic.show_string("You have " + str(mctnb) + " mctnb.")
+input.on_pin_pressed(TouchPin.P1, on_pin_pressed_p1)
+
+"""
+
+Needed start-up functions
+
+"""
+def calculate(x: number, y: number):
+    return x / y * 100
+kelp = False
+mctnb = 0
+# Needed start-up variables
+worth = 1
+bitcoin = 30000
+percentage = calculate(worth, bitcoin)
+# Initate start-up
+basic.show_string("Welcome to MCTNB Blockchain")
+basic.show_string("0 = Mine, 1 = See Stats, 2 = Reset")
+basic.show_string("Starting now MCTNB is worth 0.003% of a bitcoin.")
--- main.ts
+++ main.ts
@@ -1,46 +1,37 @@
-// Needed start-up functions
-
-function calculate(x: number, y: number){
-    return (x / y) * 100;
-}
-
-// Needed start-up variables
-
-let worth = 1;
-let bitcoin = 30000;
-let mctnb = 0;
-let percentage = calculate(worth, bitcoin);
-let kelp = false;
-
-// Initate start-up
-
-basic.showString("Welcome to MCTNB Blockchain");
-basic.showString("0 = Mine, 1 = See Stats, 2 = Reset");
-basic.showString("Starting now MCTNB is worth 0.003% of a bitcoin.");
-
 // Controls
-
-input.onPinPressed(TouchPin.P0, function(){
-    mctnb += 0.1;
-    if(!kelp){
-        basic.showNumber(mctnb);
+input.onPinPressed(TouchPin.P0, function () {
+    mctnb += 0.1
+    if (!(kelp)) {
+        basic.showNumber(mctnb)
     }
-});
-
-input.onPinPressed(TouchPin.P1, function(){
-    basic.showString("You own " + calculate(mctnb, bitcoin).toString() + "% of bitcoin.");
-    basic.showString("You have " + mctnb.toString() + " mctnb.");
-});
-
-input.onPinPressed(TouchPin.P2, function(){
-    basic.showString("RESET");
-    mctnb = 0;
-});
-
+})
 // Button Press
-
-input.onButtonPressed(Button.A, function(){
-    basic.showString("Simple Mode");
-    basic.showString("No longer will show number.");
-    kelp = true;
-});
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Simple Mode")
+    basic.showString("No longer will show number.")
+    kelp = true
+})
+input.onPinPressed(TouchPin.P2, function () {
+    basic.showString("RESET")
+    mctnb = 0
+})
+input.onPinPressed(TouchPin.P1, function () {
+    basic.showString("You own " + calculate(mctnb, bitcoin).toString() + "% of bitcoin.")
+    basic.showString("You have " + mctnb.toString() + " mctnb.")
+})
+/**
+ * Needed start-up functions
+ */
+function calculate (x: number, y: number) {
+    return x / y * 100
+}
+let kelp = false
+let mctnb = 0
+// Needed start-up variables
+let worth = 1
+let bitcoin = 30000
+let percentage = calculate(worth, bitcoin)
+// Initate start-up
+basic.showString("Welcome to MCTNB Blockchain")
+basic.showString("0 = Mine, 1 = See Stats, 2 = Reset")
+basic.showString("Starting now MCTNB is worth 0.003% of a bitcoin.")
